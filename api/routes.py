c78319cf330b8bde9c8a36a9719c04aef8cbfbf5
Blaine Mason <bmason3@gulls.salisbury.edu>
2022-05-03 17:49:13 -0400

    Fix stats route

--- api/routes.py
+++ api/routes.py
@@ -27,7 +27,7 @@ def get_listing_stats(listing_id: int):
     # Get the listing:
     response = dict()
 
-    listing_stats = ListingsStatisticsModel.query.filter_by(id=listing_id)\
+    listing_stats = ListingsStatisticsModel.query.filter_by(listing_id=listing_id)\
         .first()
 
     # If the listing doesn't exist, return 404:
