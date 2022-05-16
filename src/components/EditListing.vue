d9354de327e945bf86e938c206b00c699f57fb08
jacobduncan00 <jacobduncan00@yahoo.com>
2022-05-16 10:25:19 -0400

    fix again

--- src/components/EditListing.vue
+++ src/components/EditListing.vue
@@ -200,31 +200,6 @@
           </div>
         </label>
       </div>
-      <div
-        v-if="statusString == 'pending'"
-        class="flex items-center justify-center w-full mb-12"
-      >
-        <label for="toogleA" class="flex items-center cursor-pointer">
-          <!-- toggle -->
-          <div class="relative">
-            <!-- input -->
-            <input
-              id="toogleA"
-              type="checkbox"
-              class="sr-only"
-              @click="set_active"
-            />
-            <!-- line -->
-            <div class="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
-            <!-- dot -->
-            <div
-              class="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"
-            ></div>
-          </div>
-          <!-- label -->
-          <div class="ml-3 text-gray-700 font-medium">Activate Listing</div>
-        </label>
-      </div>
       <button
         type="button"
         class="bg-primary hover:bg-primaryOffset text-white font-bold py-2 px-4 w-1/3 m-auto rounded flex justify-center items-center"
